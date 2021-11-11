ea87b23b0ec9b05e64066a99e3e47d812f00f6ad
Gabriele Rinaldis <32817747+rigabri@users.noreply.github.com>
2021-11-11 10:17:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int f(int n) {
+   if (n == 1) return 1;
+   return n + f(n-1);
+}
+
 int main() {
+   int n = 0;
+   cin >> n;
+   if (n <= 2)
+      cout << "errore";
+   else
+      cout << f(n);
    return 0;
 }
